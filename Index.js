9ce07c5f56b9db9d0ae30c447f5603981bd31d4c
Rúni Bjartalið <runi0204@edu.zealand.dk>
2022-11-23 14:20:47 +0100

    Mapper er nu oprettet

---
+++ Index.js
@@ -0,0 +1,16 @@
+const baseUri =""
+Vue.createApp({
+    return{
+        NewList:[],
+        smartPhoneList:[],
+        error:[],
+        statuscode:[],
+        getSmrtPhoneId: "",
+        smaerphone:8,
+        smartphoneModel:"Iphone",
+        smartPhonePrice:10000,
+        smartPhoneDescription:"denne telefon er awsome",
+        smartPhoneOperativSystem:"IOS15",
+        deleteSmartPhoneId:1
+    }
+})
